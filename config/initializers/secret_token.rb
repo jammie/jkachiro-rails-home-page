64bc4a1ceeb027ad1ebeb60adbb175b9891dcdcf
James Kachiro Sarumaha <merouzhiro@gmail.com>
2011-10-28 11:52:03 +0700

    first release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JkachiroRailsHomePage::Application.config.secret_token = 'e6769c878f4388e03827e43f2708e44bc597a185c7b73bde5bbcb3613c5e673c3b1f1757da481b109e7ccad1d7097f151e3f1eb489ae0f8a5d58990338b2a87b'
